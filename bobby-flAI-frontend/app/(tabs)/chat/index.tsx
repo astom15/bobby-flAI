447874326d017fed560f6377c6f68ef38b61abc5
Andre <andretom15@gmail.com>
2024-12-29 18:18:08 -0800

    populating messages

--- bobby-flAI-frontend/app/(tabs)/chat/index.tsx
+++ bobby-flAI-frontend/app/(tabs)/chat/index.tsx
@@ -1,11 +1,11 @@
 import {
   View,
+  Image,
   Text,
   StyleSheet,
   Pressable,
   KeyboardAvoidingView,
   Platform,
-  ScrollView,
 } from "react-native";
 import PreviousChatsModal from "./(components)/PreviousChats";
 import React, { useEffect, useState } from "react";
@@ -15,17 +15,41 @@ import { useLocalSearchParams } from "expo-router";
 import MessageInput from "@/app/components/MessageInput";
 // eslint-disable-next-line import/no-unresolved
 import { defaultStyles } from "@/app/utils/Styles";
+// eslint-disable-next-line import/no-unresolved
+import { Message, Role } from "@/app/types/messages";
+import { FlashList } from "@shopify/flash-list";
+// eslint-disable-next-line import/no-unresolved
+import ChatMessage from "@/app/components/ChatMessage";
 
 const previousChats: Chat[] = [
   { id: "1", name: "What can I make with cheese and eggs" },
   { id: "2", name: "What's a good gnocchi recipe" },
   { id: "3", name: "Burger King at home" },
 ];
-
+const DUMMY_MESSAGES: Message[] = [
+  {
+    role: Role.bot,
+    content: "hello, how can i help you today",
+  },
+  {
+    role: Role.user,
+    content: "I need help with my cooking assistant app",
+  },
+  {
+    role: Role.bot,
+    content: "Sure, how can i help you",
+  },
+  {
+    role: Role.user,
+    content: "I need to cook something with these ingredients",
+  },
+];
 const ChatBot = () => {
   const { isNewChat } = useLocalSearchParams();
+  const [height, setHeight] = useState(0);
   const [isModalVisible, setModalVisible] = useState(false);
   const [currentChat, setCurrentChat] = useState<Chat | null>(null);
+  const [messages, getMessages] = useState<Message[]>(DUMMY_MESSAGES);
 
   useEffect(() => {
     if (isNewChat) {
@@ -36,6 +60,11 @@ const ChatBot = () => {
   const handleChatSelect = (chat: Chat) => {
     setCurrentChat(chat);
     setModalVisible(false);
+  };
+
+  const onLayout = (event: any) => {
+    const { height } = event.nativeEvent.layout;
+    setHeight(height / 2);
   };
 
   const getCompleteMessage = async (message: string) => {
@@ -49,21 +78,24 @@ const ChatBot = () => {
         chats={previousChats}
         onSelect={handleChatSelect}
       />
-      {currentChat ? (
-        <>
-          <Text style={styles.header}>{currentChat.name} </Text>
-          <Pressable
-            style={styles.chatButton}
-            onPress={() => setModalVisible(true)}
-          >
-            <Text style={styles.buttonText}>Chats</Text>
-          </Pressable>
-        </>
-      ) : (
-        <>
-          <Text style={styles.header}>Let's Start Cooking</Text>
-        </>
-      )}
+      <View style={styles.page} onLayout={onLayout}>
+        {messages.length === 0 && (
+          <View style={[styles.logoContainer, { marginTop: height / 2 - 100 }]}>
+            <Image
+              source={require("../../../assets/images/react-logo.png")}
+              style={styles.image}
+            />
+          </View>
+        )}
+        <FlashList
+          data={messages}
+          estimatedItemSize={400}
+          contentContainerStyle={{ paddingTop: 30, paddingBottom: 150 }}
+          keyboardDismissMode="on-drag"
+          renderItem={({ item }) => <ChatMessage {...item} />}
+        />
+      </View>
+
       <KeyboardAvoidingView
         style={[styles.keyboardStyle]}
         keyboardVerticalOffset={60}
@@ -94,6 +126,24 @@ const styles = StyleSheet.create({
     left: 0,
     bottom: 0,
     //flexShrink: 0,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    resizeMode: "cover",
+  },
+  logoContainer: {
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+    backgroundColor: "#000",
+    borderRadius: 50,
+  },
+  page: {
+    flex: 1,
+    backgroundColor: "red",
   },
 });
 
--- bobby-flAI-frontend/app/types/messages.ts
+++ bobby-flAI-frontend/app/types/messages.ts
@@ -1,3 +1,15 @@
 export interface MessageInputProps {
   onShouldSendMessage: (message: string) => void;
 }
+
+export enum Role {
+  user = 1,
+  bot = 0,
+}
+
+export interface Message {
+  role: Role;
+  content: string;
+  imageUrl?: string;
+  prompt?: string;
+}
